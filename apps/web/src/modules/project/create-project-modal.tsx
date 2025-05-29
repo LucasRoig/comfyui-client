7b701f3dc39a95bcc9fe80e1a542aeb773fbb592
Lucas Roig <lucas1.roig@gmail.com>
2025-05-29 11:11:46 +0200

    feat(create project): basic form setup

--- apps/web/src/modules/project/create-project-modal.tsx
+++ apps/web/src/modules/project/create-project-modal.tsx
@@ -1,25 +1,74 @@
 "use client";
 import { Button } from "@lro-ui/button";
 import { Modal, ModalBody, ModalClose, ModalContent, ModalFooter, ModalHeader, ModalTitle } from "@lro-ui/modal";
+import { type AnyFieldApi, useForm } from "@tanstack/react-form";
 
 type CreateProjectModalProps = {
   isOpen: boolean;
   onOpenChange: (isOpen: boolean) => void;
 };
 
+function FieldInfo({ field }: { field: AnyFieldApi }) {
+  return (
+    <>
+      {field.state.meta.isTouched && !field.state.meta.isValid ? (
+        <em>{field.state.meta.errors.map((err) => err.message).join(",")}</em>
+      ) : null}
+      {field.state.meta.isValidating ? "Validating..." : null}
+    </>
+  );
+}
+
 export function CreateProjectModal(props: CreateProjectModalProps) {
+  const form = useForm({
+    defaultValues: {
+      name: "",
+    },
+    onSubmit: (event) => {
+      console.log(event.value);
+    },
+  });
   return (
     <Modal open={props.isOpen} onOpenChange={props.onOpenChange}>
       <ModalContent className="max-w-2xl">
         <ModalHeader>
           <ModalTitle>Create a new project</ModalTitle>
         </ModalHeader>
-        <ModalBody />
+        <ModalBody>
+          <form
+            id="create-project-form"
+            onSubmit={(e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              form.handleSubmit(e);
+            }}
+          >
+            <form.Field name="name">
+              {(field) => {
+                return (
+                  <>
+                    <label htmlFor={field.name}>First Name:</label>
+                    <input
+                      id={field.name}
+                      name={field.name}
+                      value={field.state.value}
+                      onBlur={field.handleBlur}
+                      onChange={(e) => field.handleChange(e.target.value)}
+                    />
+                    <FieldInfo field={field} />
+                  </>
+                );
+              }}
+            </form.Field>
+          </form>
+        </ModalBody>
         <ModalFooter>
           <ModalClose asChild>
             <Button variant="text">Cancel</Button>
           </ModalClose>
-          <Button>Create</Button>
+          <Button type="submit" form="create-project-form">
+            Create Project
+          </Button>
         </ModalFooter>
       </ModalContent>
     </Modal>
