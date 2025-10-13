ded5e03922ead478c2f3eecbde68df8fe66b4063
Lucas Roig <lucas1.roig@gmail.com>
2025-10-13 17:38:49 +0200

    feat: add pagination and selection to table

--- apps/web/src/app/projects/[projectId]/files/page.tsx
+++ apps/web/src/app/projects/[projectId]/files/page.tsx
@@ -1,8 +1,10 @@
 "use client";
+import { Checkbox, UndeterminateCheckbox } from "@lro-ui/input";
 import { Skeleton } from "@lro-ui/skeleton";
 import { DataTable } from "@lro-ui/table";
 import { useQuery } from "@tanstack/react-query";
 import type { ColumnDef } from "@tanstack/react-table";
+import { useState } from "react";
 import { useSelectedProject } from "../../../../modules/project/selected-project-context";
 import { orpc } from "../../../../orpc/link";
 import type { RouterOutputs } from "../../../../orpc/router";
@@ -10,6 +12,29 @@ import type { RouterOutputs } from "../../../../orpc/router";
 type InputImage = RouterOutputs["project"]["getFilesInProject"][number];
 
 const columns = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <UndeterminateCheckbox
+        checked={table.getIsAllPageRowsSelected() || (table.getIsSomePageRowsSelected() && "indeterminate")}
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+        className="translate-y-[2px]"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => {
+          row.toggleSelected(!!value);
+        }}
+        aria-label="Select row"
+        className="translate-y-[2px]"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
   {
     header: "Name",
     accessorFn: (row) => row.originalFileName,
@@ -21,6 +46,7 @@ const columns = [
 ] satisfies ColumnDef<InputImage>[];
 
 export default function FilesPage() {
+  const [rowSelection, setRowSelection] = useState({});
   const projectInfo = useSelectedProject();
   const {
     data: inputImages,
@@ -47,7 +73,12 @@ export default function FilesPage() {
       ) : inputImages === undefined ? (
         <div>Unknown Error</div>
       ) : (
-        <DataTable columns={columns} data={inputImages} />
+        <DataTable
+          stickyHeader
+          columns={columns}
+          data={inputImages}
+          rowSelectionModel={{ rowSelection, setRowSelection }}
+        />
       )}
     </>
   );
