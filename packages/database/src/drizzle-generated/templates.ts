3e818e82c5ca7fbce6be40306e5e3f7b47580730
Lucas Roig <lucas1.roig@gmail.com>
2025-12-07 13:02:22 +0100

    chore: update package.json commands

--- packages/database/src/drizzle-generated/templates.ts
+++ packages/database/src/drizzle-generated/templates.ts
@@ -1,9 +1,3 @@
-import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
 
-export const templates = sqliteTable("template", {
-  id: text("id").primaryKey(),
-  name: text("name").notNull(),
-  isRoot: integer("is_root", { mode: "boolean" }).default(false).notNull(),
-  parentId: text("parent_id"),
-  projectId: text("project_id").notNull(),
-});
+export const templates = sqliteTable('template', { id: text('id').primaryKey(), name: text('name').notNull(), isRoot: integer('is_root', { mode: 'boolean' }).default(false).notNull(), parentId: text('parent_id'), projectId: text('project_id').notNull() });
